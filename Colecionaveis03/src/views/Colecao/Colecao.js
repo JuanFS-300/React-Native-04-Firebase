895c19a33e934cc13582c6b34cb56666dd0fd5fe
RhaissaZeferino <rhanalialves@gmail.com>
2020-11-16 02:51:50 +0000

    Firestore-Parte01
    Signed-off-by: RhaissaZeferino <rhanalialves@gmail.com>

--- Colecionaveis03/src/views/Colecao/Colecao.js
+++ Colecionaveis03/src/views/Colecao/Colecao.js
@@ -46,6 +46,14 @@ function Colecao({ navigation }) {
         navigation.navigate('Inicial')
     }
     
+    const adicionar = () => {
+        navigation.navigate('Item', {item:{}, operacao: 'adicionar'})
+    }
+    
+    const editar = (item) => {
+        navigation.navigate('Item', {item: item, operacao: 'editar'})
+    }
+    
     return (
         <View style={estiloColecao.container}>
 
@@ -53,15 +61,19 @@ function Colecao({ navigation }) {
                 <TouchableOpacity onPress={voltar}>
                     <MaterialIcons name="arrow-back" size={24} color="white" />
                 </TouchableOpacity>
+
                 <Text style={estiloColecao.texto}>Coleção</Text>
+
+                <TouchableOpacity onPress={adicionar}>                
                 <MaterialIcons name="add" size={24} color="white" />
+                </TouchableOpacity>
             </View>
 
             <FlatList 
                 showsHorizontalScrollIndicator={false}
                 keyExtractor={(item) => item.id}
                 data={colecao}
-                renderItem={ ({item}) => <ItemLista data={item} />}
+                renderItem={ ({item}) => <ItemLista data={item} detalhe={() => editar(item)}/>}
             />
 
         </View>
